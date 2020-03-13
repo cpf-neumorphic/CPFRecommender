8830be5c8322700bd4689caf800d42123b04e2b0
Okkar Min <OMIN001@e.ntu.edu.sg>
2020-03-13 15:08:44 +0800

    Add job industiry and gender earning to job_industires.py

---
+++ job_industries.py
@@ -0,0 +1,17 @@
+job_industries = {
+    "manufacturing": {"male": 5070, "female": 3900},
+    "construction": {"male": 4241, "female": 3803},
+    "wholesale_retail_trade": {"male": 4505, "female": 3675},
+    "transportation_storage": {"male": 3000, "female": 3803},
+    "accommodation_food_services": {"male": 2560, "female": 2008},
+    "information_communications": {"male": 6435, "female": 5070},
+    "financial_insurance_services": {"male": 7583, "female": 6093},
+    "real_estate_services": {"male": 4706, "female": 4212},
+    "professional_services": {"male": 6110, "female": 5070},
+    "administrative_support_services": {"male": 2535, "female": 2583},
+    "public_administration_education": {"male": 6581, "female": 5485},
+    "heal_social_services": {"male": 6143, "female": 3803},
+    "arts_entertainment_recreation": {"male": 3955, "female": 3422},
+    "others": {"male": 4332, "female": 4377},
+}
+
