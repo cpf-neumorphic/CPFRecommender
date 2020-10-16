01d37f39a78660f7c86033a0e6fb8c45b94e86fb
Okkar Min <OMIN001@e.ntu.edu.sg>
2020-10-16 20:04:35 +0800

    Change to emoji in streamlit_barbra_martin.py

--- streamlit_barbra_martin.py
+++ streamlit_barbra_martin.py
@@ -24,9 +24,9 @@ barbra_age = st.select_slider("Barbra Martin's age", options=[25, 31, 49, 56, "a
 st.write("Barbra Martin")
 
 col1, col2, col3 = st.beta_columns(3)  # Layout features in 3 columns
-col1.write(f"age: {barbra_age}")
-col2.write(f"income: {barbra_martin_features[barbra_age][0]}")
-col3.write(f"children: {barbra_martin_features[barbra_age][1]}")
+col1.write(f"👱🏻‍♀️: {barbra_age} years old")
+col2.write(f"📥 💵: {barbra_martin_features[barbra_age][0]}")
+col3.write(f"👨‍👩‍👧‍👧: {barbra_martin_features[barbra_age][1]} children")
 
 # Barbra Martin's recommendations by age
 st.write("")
@@ -38,7 +38,7 @@ rec1, rec2, rec3 = barbra_martin_recommendations[barbra_age]
 col1, col2, col3 = st.beta_columns(3)  # Layout recommendations in 3 columns
 col1.write("1st Recommendation : ")
 col1.image(Image.open(f"img/{rec1}.png"), use_column_width=True)
-col2.write("2nd Recommendation : ")
+col2.write("2nd : ")
 col2.image(Image.open(f"img/{rec2}.png"), use_column_width=True)
-col3.write("3rd Recommendation : ")
+col3.write("3rd : ")
 col3.image(Image.open(f"img/{rec3}.png"), use_column_width=True)
