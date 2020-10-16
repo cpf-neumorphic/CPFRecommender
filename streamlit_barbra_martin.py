a3c2458a46627b8b42fe35871b25738b54b1e3c7
Okkar Min <OMIN001@e.ntu.edu.sg>
2020-10-16 19:46:10 +0800

    Add Barbra Martin's streamlit

---
+++ streamlit_barbra_martin.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+# Generate mock data
+barbra_martin_recommendations = {
+    25: ["transactions", "appointments", "giro"],
+    31: ["demo", "contributions", "contact"],
+    49: ["education", "contact", "contributions"],
+    56: ["retirements", "e-medical", "appointments"],
+}
+
+barbra_martin_features = {
+    25: ["S$3673", "0"],
+    31: ["S$4872", "0"],
+    49: ["S$5533", "2"],
+    56: ["S$1000", "2"],
+}
+
+# Streamlit's slider
+barbra_age = st.select_slider("Barbra Martin's age", options=[25, 31, 49, 56, "all"])
+
+# Barbra Martin's features by age
+st.write("Barbra Martin")
+
+col1, col2, col3 = st.beta_columns(3)  # Layout features in 3 columns
+col1.write(f"age: {barbra_age}")
+col2.write(f"income: {barbra_martin_features[barbra_age][0]}")
+col3.write(f"children: {barbra_martin_features[barbra_age][1]}")
+
+# Barbra Martin's recommendations by age
+st.write("")
+st.write("")
+st.write("page recommendations for Barbra Martin")
+
+rec1, rec2, rec3 = barbra_martin_recommendations[barbra_age]
+
+col1, col2, col3 = st.beta_columns(3)  # Layout recommendations in 3 columns
+col1.write("1st Recommendation : ")
+col1.image(Image.open(f"img/{rec1}.png"), use_column_width=True)
+col2.write("2nd Recommendation : ")
+col2.image(Image.open(f"img/{rec2}.png"), use_column_width=True)
+col3.write("3rd Recommendation : ")
+col3.image(Image.open(f"img/{rec3}.png"), use_column_width=True)
