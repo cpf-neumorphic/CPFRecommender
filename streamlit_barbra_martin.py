8c503163f5e3956fca0d5128e14aaa7b0bbb594e
OkkarMin <phoehtaungwin@gmail.com>
2020-10-20 10:32:50 +0800

    Add plotly chart w/ data

--- streamlit_barbra_martin.py
+++ streamlit_barbra_martin.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 from PIL import Image
@@ -26,25 +27,35 @@ st.beta_set_page_config(
 # Streamlit's slider
 barbra_age = st.select_slider("Barbra Martin's age", options=[25, 31, 49, 56, "all"])
 
-# Barbra Martin's features by age
-st.write("Barbra Martin")
+try:
+    # Barbra Martin's features by age
+    st.write("Barbra Martin")
 
-col1, col2, col3 = st.beta_columns(3)  # Layout features in 3 columns
-col1.write(f"👱🏻‍♀️: **{barbra_age} years old**")
-col2.write(f"💵: **{barbra_martin_features[barbra_age][0]}**")
-col3.write(f"👨‍👩‍👧‍👧: **{barbra_martin_features[barbra_age][1]}**")
+    col1, col2, col3 = st.beta_columns(3)  # Layout features in 3 columns
+    col1.write(f"👱🏻‍♀️: **{barbra_age} years old**")
+    col2.write(f"💵: **{barbra_martin_features[barbra_age][0]}**")
+    col3.write(f"👨‍👩‍👧‍👧: **{barbra_martin_features[barbra_age][1]}**")
 
-# Barbra Martin's recommendations by age
-st.write("")
-st.write("")
-st.write("Page recommendations for Barbra Martin")
+    # Barbra Martin's recommendations by age
+    st.write("")
+    st.write("")
+    st.write("Page recommendations for Barbra Martin")
 
-rec1, rec2, rec3 = barbra_martin_recommendations[barbra_age]
+    rec1, rec2, rec3 = barbra_martin_recommendations[barbra_age]
 
-col1, col2, col3 = st.beta_columns(3)  # Layout recommendations in 3 columns
-col1.write("1st Recommendation : ")
-col1.image(Image.open(f"img/{rec1}.png"), use_column_width=True)
-col2.write("2nd : ")
-col2.image(Image.open(f"img/{rec2}.png"), use_column_width=True)
-col3.write("3rd : ")
-col3.image(Image.open(f"img/{rec3}.png"), use_column_width=True)
+    col1, col2, col3 = st.beta_columns(3)  # Layout recommendations in 3 columns
+    col1.write("1st Recommendation : ")
+    col1.image(Image.open(f"img/{rec1}.png"), use_column_width=True)
+    col2.write("2nd : ")
+    col2.image(Image.open(f"img/{rec2}.png"), use_column_width=True)
+    col3.write("3rd : ")
+    col3.image(Image.open(f"img/{rec3}.png"), use_column_width=True)
+except:
+    df = pd.read_csv("./barbra_martin_chart_data.csv")
+    fig = px.scatter(df, x="age", y="page", color="recommendation",
+            title="Barbra Martin's stages of life on CPF Portal")
+    fig.update_traces(marker=dict(size=12,line=dict(width=2, color="DarkSlateGrey")),
+            selector=dict(mode="markers"))
+
+    st.plotly_chart(fig)
+
